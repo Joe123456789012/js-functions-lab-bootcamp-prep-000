5bf737b3f4c5a18746ac89bd01f6514f29ce72f5
koho20002000@yahoo.com Berryman <koho20002000@yahoo.com>
2018-07-07 04:53:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ function happyHolidaysTo(holiday,name){
 function happyHolidaysTo(days,holiday){
   return 'Its ${days} days until ${holiday}!';
 }
+//1. Write a function that returns a value
+function happyHolidays(){
+  return "Happy holidays!";
+}
+//Write a function that takes in a parameter
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`;
+}
+//Write a function that takes in multiple parameters
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+}
+//Write a function that takes in multiple parameters
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`;
+}
